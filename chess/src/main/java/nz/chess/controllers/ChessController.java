e44c723329cb75e093dce7955b0b35aacfb6c60d
Yuichi <youichy0945@gmail.com>
2024-06-30 18:57:49 +1200

    created the starting board in fxml and added the ChessController class

---
+++ chess/src/main/java/nz/chess/controllers/ChessController.java
@@ -0,0 +1,8 @@
+package nz.chess.controllers;
+
+public class ChessController {
+    
+    public void initialize() {
+
+    }
+}
---
+++ chess/src/main/java/nz/chess/pieces/Rook.java
@@ -0,0 +1,5 @@
+package nz.chess.pieces;
+
+public class Rook extends Piece{
+    
+}
