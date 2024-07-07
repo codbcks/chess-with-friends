d4f1a746c3117880cd6f8f3158f427e22d4a76e1
codbcks <euchan1234@gmail.com>
2024-07-07 19:37:47 +1200

    added castling

--- chess/src/main/java/nz/chess/pieces/King.java
+++ chess/src/main/java/nz/chess/pieces/King.java
@@ -2,12 +2,16 @@ package nz.chess.pieces;
 
 import javafx.scene.image.ImageView;
 import nz.chess.Square;
+import nz.chess.controllers.ChessController;
 import nz.chess.controllers.ChessController.moveType;
 
 public class King extends Piece{
 
+    private boolean hasMoved;
+
     public King(boolean isWhite, ImageView image) {
         super(isWhite, image);
+        this.hasMoved = false;
     }
     
     @Override
@@ -17,6 +21,34 @@ public class King extends Piece{
                 return moveType.NORMAL;
             }
         }
+
+        if (!hasMoved && currentY == targetY && Math.abs(currentX - targetX) == 2) {
+            int direction = (targetX - currentX) / 2;
+            if (isValidCastle(currentX, currentY, direction, board)) {
+                return moveType.CASTLE;
+            }
+        }
+
         return moveType.INVALID;
+    }
+
+    private boolean isValidCastle(int x, int y, int direction, Square[][] board) {
+        int rookX = (direction == -1) ? 0 : 7;
+        Piece rook = board[rookX][y].getPiece();
+        if (rook instanceof Rook && !((Rook) rook).hasMoved()) {
+            // Check if inbetween there are any blocking pieces or if the king is in check
+            for (int i = 1; i < Math.abs(rookX - x); i++) {
+                if (board[x + i * direction][y].getPiece() != null) {
+                    return false;
+                }
+            }
+
+            return !ChessController.isInCheck(isWhite()) && !ChessController.isInCheckAfterMove(x, y, x + direction, y, isWhite()) && !ChessController.isInCheckAfterMove(x, y, x + 2 * direction, y, isWhite());
+        }
+        return false;
+    }
+
+    public void setHasMoved(boolean hasMoved) {
+        this.hasMoved = hasMoved;
     }
 }
